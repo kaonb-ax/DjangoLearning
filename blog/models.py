322ea626bd941e52c830696b6323751080840cd5
atax <ad.joblink@gmail.com>
2017-07-03 13:41:32 +0200

    second commit

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Article(models.Model):
+    titre = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False,
+                                verbose_name="Date de parution")
+
+    def __str__(self):
+        """
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que
+        nous traiterons plus tard et dans l'administration
+        """
+        return self.titre
 # Create your models here.
